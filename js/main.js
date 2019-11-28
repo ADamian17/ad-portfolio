511f05a34aff9880be07799915905b05a38d5506
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-11-28 11:48:53 -0800

    some Style

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 console.log("hello Adonis");
+
+$(document).ready(function(){
+  console.log('here')
+  $(window).scroll(function(){
+  	let scroll = $(window).scrollTop();
+	  if (scroll > 50) {
+	    $(".nav-container").css("background" , "rgba(0,0,0,0.5)");
+	  }
+	  else{
+		  $(".nav-container").css("background" , "transparent");  	
+	  }
+  })
+})
