df6dff7101accdb5935a89191193dacc0e54ed96
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-11-30 12:14:55 -0800

    working in the layout

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 console.log("hello Adonis");
 
 $(document).ready(function(){
-  console.log('here')
   $(window).scroll(function(){
   	let scroll = $(window).scrollTop();
 	  if (scroll > 50) {
-	    $(".nav-container").css("background" , "rgba(0,0,0,0.5)");
+	    $(".navbar").css("background" , "rgba(0,0,0,0.5) !important");
 	  }
 	  else{
-		  $(".nav-container").css("background" , "transparent");  	
+		  $(".navbar ").css("background" , "transparent !important");  	
 	  }
   })
 })
