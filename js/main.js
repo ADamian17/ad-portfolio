4282381c37bf2cd22b978ca2834af7aa335a470d
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-12-02 07:35:21 -0800

    added more style

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const projects = [
-	{title: "Burning Rubber",  description: "this is a racing game Akskjdcskcnkkc ksdjckjckscnkk", repoLink: "https://git.generalassemb.ly/ADamian17/burnning_rubber", gameLink: "https://pages.git.generalassemb.ly/ADamian17/burnning_rubber/", photo:"./images/burnning_rubber.png"},
-	{title: "Burning Rubber",  description: "this is a racing game Akskjdcskcnkkc ksdjckjckscnkk", repoLink: "https://git.generalassemb.ly/ADamian17/burnning_rubber", gameLink: "https://pages.git.generalassemb.ly/ADamian17/burnning_rubber/", photo:"./images/burnning_rubber.png"},
-]
+	{title: "Burning Rubber",  description: "this is a racing game Inspired in an old race game i used to play", repoLink: "https://git.generalassemb.ly/ADamian17/burnning_rubber", gameLink: "https://pages.git.generalassemb.ly/ADamian17/burnning_rubber/", photo:"./images/burnning_rubber.png"},
+	{title: "Burning Rubber",  description: "this is a racing game Inspired in an old race game i used to play", repoLink: "https://git.generalassemb.ly/ADamian17/burnning_rubber", gameLink: "https://pages.git.generalassemb.ly/ADamian17/burnning_rubber/", photo:"./images/burnning_rubber.png"},
+];
 
 // functions
 
@@ -13,8 +13,8 @@ const showProjects = (arr) => {
 		const template = 
 		`	<div class="col-12 col-md-6 col-sm-6 col-lg-6 pb-5 projects-holder">
 				<div class="card">
-				<img src=${arr[i].photo} class="card-img-top" alt="...">
-				<div class="card-body">
+				<img src=${arr[i].photo} class="card-img-top" alt="photo">
+				<div class="card-body flex-fill">
 					<h5 class="card-title"><a href=${arr[i].repoLink} target="_blank">${arr[i].title}.</a></h5>
 					<p class="card-text">${arr[i].description}.</p>
 					<a href=${arr[i].gameLink} class="btn btn-primary rounded-pill float-right">Demo</a>
