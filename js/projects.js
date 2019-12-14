d850c9260c525c1e9b2bcc6dac52c33d032de122
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-12-13 17:24:49 -0800

    testing

--- js/projects.js
+++ js/projects.js
@@ -13,19 +13,5 @@ const projects = [
   "repoLink": "https://git.generalassemb.ly/ADamian17/burnning_rubber", 
   "gameLink": "https://pages.git.generalassemb.ly/ADamian17/burnning_rubber/", 
   "photo":"./images/burnning_rubber.png"
-}, 
-  {
-  "title": "Burning Rubber",  
-  "description": "this is a racing game Inspired in an old race game i used to play", 
-  "repoLink": "https://git.generalassemb.ly/ADamian17/burnning_rubber", 
-  "gameLink": "https://pages.git.generalassemb.ly/ADamian17/burnning_rubber/", 
-  "photo":"/images/burnning_rubber.png"
-}, 
-  {
-  "title": "Burning Rubber",  
-  "description": "this is a racing game Inspired in an old race game i used to play", 
-  "repoLink": "https://git.generalassemb.ly/ADamian17/burnning_rubber", 
-  "gameLink": "https://pages.git.generalassemb.ly/ADamian17/burnning_rubber/", 
-  "photo":"../images/burnning_rubber.png"
-} 
+}
 ]
