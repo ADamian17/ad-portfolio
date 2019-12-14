67b4bb0b031357488e00c410347862a15a2c719a
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-12-13 16:54:42 -0800

    added img

--- js/projects.js
+++ js/projects.js
@@ -4,9 +4,23 @@ const projects = [
   "description": "this is a react app inspire by Hulu", 
   "repoLink": "https://git.generalassemb.ly/ADamian17/burnning_rubber", 
   "demoLink": "https://watchchulo.herokuapp.com/movies", 
-  "photo":"../images/chulo3.png"
+  "photo":"/images/chulo3.png"
 },
 
+  {
+  "title": "Burning Rubber",  
+  "description": "this is a racing game Inspired in an old race game i used to play", 
+  "repoLink": "https://git.generalassemb.ly/ADamian17/burnning_rubber", 
+  "gameLink": "https://pages.git.generalassemb.ly/ADamian17/burnning_rubber/", 
+  "photo":"/images/burnning_rubber.png"
+}, 
+  {
+  "title": "Burning Rubber",  
+  "description": "this is a racing game Inspired in an old race game i used to play", 
+  "repoLink": "https://git.generalassemb.ly/ADamian17/burnning_rubber", 
+  "gameLink": "https://pages.git.generalassemb.ly/ADamian17/burnning_rubber/", 
+  "photo":"/images/burnning_rubber.png"
+}, 
   {
   "title": "Burning Rubber",  
   "description": "this is a racing game Inspired in an old race game i used to play", 
