95d6675bbb4948775a487a05f3120fadf4faa79c
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-12-17 13:58:37 -0800

    working on project description

--- js/projects.js
+++ js/projects.js
@@ -1,16 +1,16 @@
 const projects = [
   {
+    "title": "Burning Rubber",  
+    "description": "this is a racing game Inspired in an old race game i used to play", 
+    "repoLink": "https://git.generalassemb.ly/ADamian17/burnning_rubber", 
+    "demoLink": "https://pages.git.generalassemb.ly/ADamian17/burnning_rubber", 
+    "photo":"./images/burnningrubber.png"
+  },
+  {
   "title": "WatchChulo",  
   "description": "this is a react app inspire by Hulu", 
   "repoLink": "https://github.com/ADamian17/chulo-front-end", 
-  "demoLink": "https://watchchulo.herokuapp.com/movies", 
+  "demoLink": "https://watchchulo.herokuapp.com", 
   "photo":"./images/chulo.png"
-},
-  {
-  "title": "Burning Rubber",  
-  "description": "this is a racing game Inspired in an old race game i used to play", 
-  "repoLink": "https://git.generalassemb.ly/ADamian17/burnning_rubber", 
-  "demoLink": "https://pages.git.generalassemb.ly/ADamian17/burnning_rubber", 
-  "photo":"./images/burnningrubber.png"
 }
 ]
