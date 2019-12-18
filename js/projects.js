3ab008016d655ed7e723c19114eaccd07a4561ac
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-12-18 11:19:06 -0800

    Added description

--- js/projects.js
+++ js/projects.js
@@ -1,16 +1,23 @@
 const projects = [
   {
     "title": "Burning Rubber",  
-    "description": "this is a racing game Inspired in an old race game i used to play", 
+    "description": "This is my first project that I created. I used css animations to bring it to live, is very fun and easy game to play. Built with html/css, Javascript, and jQuery", 
     "repoLink": "https://git.generalassemb.ly/ADamian17/burnning_rubber", 
     "demoLink": "https://pages.git.generalassemb.ly/ADamian17/burnning_rubber", 
     "photo":"./images/burnningrubber.png"
   },
   {
   "title": "WatchChulo",  
-  "description": "this is a react app inspire by Hulu", 
+  "description": "This is a demo app  inspired by Hulu. I built this app myself from start to finish, using Mongodb, Express, React, Node and Bootstrap. Hosted on Heroku", 
   "repoLink": "https://github.com/ADamian17/chulo-front-end", 
   "demoLink": "https://watchchulo.herokuapp.com", 
   "photo":"./images/chulo.png"
-}
+  },
+  {
+  "title": "Wayfarer",  
+  "description": "This is a four people team project. While i focused  on the front-end i was able to also implement some of the features in the back-end.   This project was built with MERN(Mongodb, Express, React, Node) stack . Hosted in Heroku", 
+  "repoLink": "https://git.generalassemb.ly/jbui/wayfarer-frontend", 
+  "demoLink": "https://wayfarer-damnbros.herokuapp.com", 
+  "photo":"./images/wayfarer.png"
+  }
 ]
