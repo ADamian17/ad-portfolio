0c4e842656ae4edcbf106e1e3d2b4870292f2e5c
ADamian17 <adonisdesuvikdo@gmail.com>
2020-08-07 16:55:32 -0700

    projects is done

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
 // functions
-const showProjects = (arr) => {
+const showProjects = ( arr ) => {
 	const $projectList = $('#project-list')
 	for (let i = 0; i < arr.length; i++) {
 		const template = 
-		`	<div class="col-12 col-md-6 col-sm-6 col-lg-6 pb-5 projects-holder">
-				<div class="card">
-				<img src=${arr[i].photo} class="card-img-top" alt="photo">
-				<div class="card-body flex-fill">
-					<h5 class="card-title">${arr[i].title}</h5>
-					<p class="card-text">${arr[i].description}.</p>
-					<a href=${arr[i].repoLink} class="btn btn-primary rounded-pill float-right" target="_blank">Code</a>
-					<a href=${arr[i].demoLink} class="btn btn-primary rounded-pill float-right mr-2" target="_blank">Demo</a>
-				 </div>
-			  </div>
+		`	<div class="col-12 col-md-6 col-sm-6 col-lg-6 projects-holder">
+				<div class="card project-card">
+					<img class="project-card__img card-img-top" src=${arr[i].photo} alt="Person in a jumping">
+					<div class="project-card__caption">
+						<h5 class="card-title">${arr[i].title}</h5>
+						<p class="card-text">${arr[i].description}.</p>
+
+						<a href=${arr[i].repoLink} class="btn btn-primary rounded-pill" target="_blank">Code</a>
+						<a href=${arr[i].demoLink} class="btn btn-primary rounded-pill mr-2" target="_blank">Demo</a>
+					</div>
+				</div>
 			 </div>
 			 `
 		$projectList.append(template)
 	}
 	
-}
+};
 
-showProjects(projects)
-
-
-
+showProjects(projects);
