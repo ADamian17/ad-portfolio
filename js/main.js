2261516ed2e3ad4adb3b7dfe4f3233b205ba8e52
ADamian17 <adonisdesuvikdo@gmail.com>
2020-08-09 08:16:17 -0700

    added skills

--- js/main.js
+++ js/main.js
@@ -22,3 +22,37 @@ const showProjects = ( arr ) => {
 };
 
 showProjects(projects);
+
+
+const skillsList = ( arr ) => {
+
+	const $skillList = $("#skills-list");
+	arr.forEach( element => {
+		console.log(element)
+		const template = `
+		<div class="card-container">
+			<div class="card-container__skill">
+				<p class="card-container__skill--text">${element.skill}: <span class="skill-card__bar"></span></p>
+			</div>
+			<div id="${element.id}" class="card-container__bar" />
+		</div>
+		`
+		$skillList.append(template);
+
+		$(`#${element.id}`).css( "background-image", `linear-gradient(-55deg, #dc8a8a ${element.level}%, red 10%)`);
+	});
+};
+
+skillsList(skills);
+
+{/* <div className="row justify-content-center">
+<div className="col-3">
+<h5 className="card-title text-center mt-2">{displayDate}</h5>
+</div>
+<div className="col-6" >
+  <div className="mt-2 pogressBar" style={bar}></div>
+</div>
+<div className="col-3">
+<h5 className="card-title mt-2 text-center">{storeCount}</h5>
+</div>
+</div> */}
---
+++ js/skills.js
@@ -0,0 +1,54 @@
+const skills = [
+    {
+        id: 1,
+        skill:"JavaScript", 
+        level: 20
+    },
+    { 
+        id: 2,
+        skill:"React/Redux",
+        level: 30
+    },
+    { 
+        id: 3,
+        skill:"NodeJS",
+        level: 40
+    },
+    {
+        id: 4,
+        skill:"Express",
+        level: 60  
+    },
+    {
+        id: 5,
+        skill:"MongoDB",
+        level: 60  
+    },
+    {
+        id: 6,
+        skill:"HTML/CSS",
+        level: 20  
+
+    },
+    {
+        id: 7,
+        skill: "JQuery",
+        level: 75  
+    },
+    {
+        id: 8,
+        skill:"Bootstrap", 
+        level: 75 
+    },
+    {
+        id: 9,
+        skill:"Sql",
+        level: 70  
+    },
+    {
+        id: 10,
+        skill: "Python",
+        level: 90
+
+    }
+];
