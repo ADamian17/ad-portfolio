affb75a9a95d990c78afe0c85a44023236e0b0cc
ADamian17 <adonisdesuvikdo@gmail.com>
2021-07-29 11:44:00 -0700

    added new project

--- js/projects.js
+++ js/projects.js
@@ -1,30 +1,37 @@
 export const projects = [
   {
+    "title": "Track That Job",  
+    "description": "This is a demo app created to keep myself organized, while job searching. I created this application myself from end to end, using React / Redux, CSS / Sass, Mongodb, Express. Hosted on Heroku", 
+    "repoLink": "https://github.com/ADamian17/job_tracker", 
+    "demoLink": "http://www.trackthatjob.com", 
+    "photo":"./images/trackThatJob.png"
+  },
+  {
+    "title": "Electron Chat App",  
+    "description": "This is a native demo app, created using Electron React/Redux and Firebase.", 
+    "repoLink": "https://github.com/ADamian17/electron-app", 
+    "demoLink": "https://adamian17.github.io/static-e/", 
+    "photo":"./images/chatapp.png"
+  },
+  {
+    "title": "WatchChulo",  
+    "description": "This is a demo app  inspired by Hulu. I built this app myself from start to finish, using Mongodb, Express, React, Node and Bootstrap. Hosted on Heroku", 
+    "repoLink": "https://github.com/ADamian17/chulo-front-end", 
+    "demoLink": "https://watchchulo.herokuapp.com", 
+    "photo":"./images/chulo2.png"
+    },
+  {
     "title": "Burning Rubber",  
     "description": "This is my first project that I created. I used css animations to bring it to live, is very fun and easy game to play. Built with html/css, Javascript, and jQuery", 
     "repoLink": "https://git.generalassemb.ly/ADamian17/burnning_rubber", 
     "demoLink": "https://burningrubber.netlify.app/", 
     "photo":"./images/burnining_ruber_new.png"
   },
-  {
-  "title": "WatchChulo",  
-  "description": "This is a demo app  inspired by Hulu. I built this app myself from start to finish, using Mongodb, Express, React, Node and Bootstrap. Hosted on Heroku", 
-  "repoLink": "https://github.com/ADamian17/chulo-front-end", 
-  "demoLink": "https://watchchulo.herokuapp.com", 
-  "photo":"./images/chulo2.png"
-  },
-  {
-  "title": "Track That Job",  
-  "description": "This is a demo app created to keep myself organized, while job searching. I created this application myself from end to end, using React / Redux, CSS / Sass, Mongodb, Express. Hosted on Heroku", 
-  "repoLink": "https://github.com/ADamian17/job_tracker", 
-  "demoLink": "http://www.trackthatjob.com", 
-  "photo":"./images/trackThatJob.png"
-  },
-  {
-  "title": "Shirt Bot",  
-  "description": "This is a ecommerce slack bot. Where you can buy t-shirts irresponsibly from your slack app. Techonologies used React/Redux, SASS. This a collaborition app between me as the Front-End Engineer and one of my friends working in the Back-End. <b>THE APP IS CURRENTLY ON DEVELOPMENT LIVE DEMO AND GITHUB REPO ARE NOT AVAILIBLE.</b>", 
-  "repoLink": null, 
-  "demoLink": null, 
-  "photo":"./images/shirtbot.png"
-  }
+  // {
+  // "title": "Shirt Bot",  
+  // "description": "This is a ecommerce slack bot. Where you can buy t-shirts irresponsibly from your slack app. Techonologies used React/Redux, SASS. This a collaborition app between me as the Front-End Engineer and one of my friends working in the Back-End. <b>THE APP IS CURRENTLY ON DEVELOPMENT LIVE DEMO AND GITHUB REPO ARE NOT AVAILIBLE.</b>", 
+  // "repoLink": null, 
+  // "demoLink": null, 
+  // "photo":"./images/shirtbot.png"
+  // }
 ]
